7a4168e5c199aab9f08c4f5b67a9405b9fafd71a
Dean Sinclair <dean.sinclair@mediatonic.co.uk>
2019-10-15 14:07:28 +0100

    Add initial hero setup

---
+++ Assets/Scripts/HeroLogic/HeroController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+
+    }
+
+    public void OnMove(InputValue value)
+    {
+        var moveDelta = value.Get<Vector2>();
+    }
+}
